4f8aa89b466a6a3653052dfe6a25049d5c92e293
bb-hajirah <u3619357@connect.hku.hk>
2024-12-03 11:13:43 +0800

    Update fileio.h

--- fileio.h
+++ fileio.h
@@ -2,19 +2,18 @@
 #define FILEIO_H
 
 #include <string>
-#include <vector>
 
 // Functions to handle file I/O for the game
 class FileIO {
 public:
     // Saves the high scores to a file
-    static void saveHighScores(const std::vector<int>& scores, const std::string& filename);
+    static void saveHighScores(int* scores, int size, const std::string& filename);
 
     // Loads the high scores from a file
-    static std::vector<int> loadHighScores(const std::string& filename);
+    static int* loadHighScores(const std::string& filename, int& size);
 
     // A utility function to display high scores
-    static void displayHighScores(const std::vector<int>& scores);
+    static void displayHighScores(int* scores, int size);
 };
 
 #endif // FILEIO_H
