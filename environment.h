fea675a462cdfdde9c4a5b86751b35957475a5f5
https-fatima <fatima13@connect.hku.hk>
2024-12-05 01:47:52 +0800

    Update environment.h

--- environment.h
+++ environment.h
@@ -1,1 +1,19 @@
+#ifndef ENVIRONMENT_H
+#define ENVIRONMENT_H
 
+extern bool gameOver;
+extern const int width;
+extern const int height;
+extern int x, y;
+extern int appleX, appleY, bananaX, bananaY, orangeX, orangeY, chocoX, chocoY, icecreamX, icecreamY;
+extern int score;
+extern int *tailX;
+extern int *tailY;
+extern int nTail;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+extern eDirection dir;
+
+void Setup();
+void Draw();
+
+#endif
