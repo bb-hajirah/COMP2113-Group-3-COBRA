42092bdd4c9406ecb4a79af4e5711363818884aa
bb-hajirah <u3619357@connect.hku.hk>
2024-12-05 22:11:47 +0800

    Create board.h

---
+++ board.h
@@ -0,0 +1,22 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <ncurses.h>
+
+class Board {
+public:
+    Board(int width, int height);
+    ~Board();
+    void draw();
+    void drawScore(int score); // Add this method to draw the score
+    WINDOW* getWindow() const;
+    WINDOW* getScoreWindow() const; // Add this method to get the score window
+
+private:
+    int width, height;
+    int startX, startY;
+    WINDOW* win;
+    WINDOW* scoreWin; // Add a window for the score
+};
+
+#endif”
