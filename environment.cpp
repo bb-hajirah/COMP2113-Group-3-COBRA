290167458ca4226d4fa6f85b6b684b0218ddc09e
SaahilAlam1 <136902134+SaahilAlam1@users.noreply.github.com>
2024-12-05 21:35:24 +0800

    Update environment.cpp

--- environment.cpp
+++ environment.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 bool gameOver;
-const int width = 20;
+const int width = 40;
 const int height = 20;
 int x, y, appleX, appleY, bananaX, bananaY, orangeX, orangeY, chocoX, chocoY, icecreamX, icecreamY, score;
 int *tailX; // Pointer for dynamic tail X coordinates
@@ -39,55 +39,76 @@ void Setup() {
     tailY = new int[100]; // Allocate memory for tail Y coordinates
 }
 
-void Draw() {
-    clear(); // Clear the screen
+void Draw(WINDOW* win) {
+    werase(win); // Clear the window
 
     // Draw top border
-    for (int i = 0; i < width + 2; i++)
-        printw("#");
-    printw("\n");
+    wattron(win, COLOR_PAIR(1));
+    mvwprintw(win, 0, 0, "+");
+    for (int i = 0; i < width; i++)
+        mvwprintw(win, 0, i + 1, "-");
+    mvwprintw(win, 0, width + 1, "+");
+    wattroff(win, COLOR_PAIR(1));
 
     // Draw game area
     for (int i = 0; i < height; i++) {
+        wattron(win, COLOR_PAIR(1));
+        mvwprintw(win, i + 1, 0, "|"); // Left border
+        wattroff(win, COLOR_PAIR(1));
         for (int j = 0; j < width; j++) {
-            if (j == 0)
-                printw("#"); // Left border
-            if (i == y && j == x)
-                printw("O"); // Snake head
-            else if (i == appleY && j == appleX)
-                printw("A"); // Apple
-            else if (i == bananaY && j == bananaX)
-                printw("B"); // Banana
-            else if (i == orangeY && j == orangeX)
-                printw("O"); // Orange
-            else if (i == chocoY && j == chocoX)
-                printw("C"); // Chocolate
-            else if (i == icecreamY && j == icecreamX)
-                printw("I"); // Icecream
-            else {
+            if (i == y && j == x) {
+                wattron(win, COLOR_PAIR(2));
+                mvwprintw(win, i + 1, j + 1, "O"); // Snake head
+                wattroff(win, COLOR_PAIR(2));
+            } else if (i == appleY && j == appleX) {
+                wattron(win, COLOR_PAIR(3));
+                mvwprintw(win, i + 1, j + 1, "A"); // Apple
+                wattroff(win, COLOR_PAIR(3));
+            } else if (i == bananaY && j == bananaX) {
+                wattron(win, COLOR_PAIR(4));
+                mvwprintw(win, i + 1, j + 1, "B"); // Banana
+                wattroff(win, COLOR_PAIR(4));
+            } else if (i == orangeY && j == orangeX) {
+                wattron(win, COLOR_PAIR(5));
+                mvwprintw(win, i + 1, j + 1, "O"); // Orange
+                wattroff(win, COLOR_PAIR(5));
+            } else if (i == chocoY && j == chocoX) {
+                wattron(win, COLOR_PAIR(6));
+                mvwprintw(win, i + 1, j + 1, "C"); // Chocolate
+                wattroff(win, COLOR_PAIR(6));
+            } else if (i == icecreamY && j == icecreamX) {
+                wattron(win, COLOR_PAIR(7));
+                mvwprintw(win, i + 1, j + 1, "I"); // Icecream
+                wattroff(win, COLOR_PAIR(7));
+            } else {
                 bool print = false;
                 for (int k = 0; k < nTail; k++) {
                     if (tailX[k] == j && tailY[k] == i) {
-                        printw("o"); // Snake tail
+                        wattron(win, COLOR_PAIR(2));
+                        mvwprintw(win, i + 1, j + 1, "o"); // Snake tail
+                        wattroff(win, COLOR_PAIR(2));
                         print = true;
                     }
                 }
-                if (!print)
-                    printw(" "); // Empty space
+                if (!print) {
+                    mvwprintw(win, i + 1, j + 1, " "); // Empty space
+                }
             }
-
-            if (j == width - 1)
-                printw("#"); // Right border
         }
-        printw("\n");
+        wattron(win, COLOR_PAIR(1));
+        mvwprintw(win, i + 1, width + 1, "|"); // Right border
+        wattroff(win, COLOR_PAIR(1));
     }
 
     // Draw bottom border
-    for (int i = 0; i < width + 2; i++)
-        printw("#");
-    printw("\n");
+    wattron(win, COLOR_PAIR(1));
+    mvwprintw(win, height + 1, 0, "+");
+    for (int i = 0; i < width; i++)
+        mvwprintw(win, height + 1, i + 1, "-");
+    mvwprintw(win, height + 1, width + 1, "+");
+    wattroff(win, COLOR_PAIR(1));
 
-    printw("Score: %d\n", score);
-    refresh(); // Refresh the screen to show changes
+    // Draw the score below the game area
+    mvwprintw(win, height + 2, 0, "Score: %d", score);
+    wrefresh(win); // Refresh the window to show changes
 }
-
