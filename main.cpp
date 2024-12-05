ef9d9990ad51aa07643c2f62d09fdef8f0be325b
https-fatima <fatima13@connect.hku.hk>
2024-12-05 13:55:35 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <ncurses.h>
+#include <cstdlib>
+#include <unistd.h>
+#include "environment.h"
+#include "game.h"
 
+int main() {
+    initscr();          // Start ncurses mode
+    noecho();           // Don't echo pressed keys
+    cbreak();           // Disable line buffering
+    curs_set(0);        // Hide the cursor
+
+    Setup();
+
+    while (!gameOver) {
+        Draw();
+        Input();
+        Logic();
+        usleep(100000); // Sleep to control speed of the game
+    }
+
+    // Clean up: free dynamically allocated memory
+    delete[] tailX;
+    delete[] tailY;
+
+    endwin();          // End ncurses mode and clean up
+
+    return 0;
+}
